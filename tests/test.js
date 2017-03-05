be7576feff9dfeb05a1ff5d5981c42580c7ab010
shahenaz Monia <shahy.m.93@gmail.com>
2017-03-06 00:30:37 +0200

    add tape,shot ,istanbul in package.json

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ const tape = require('tape');
 const shot = require('shot');
 const main = require('../main.js');
 
-shot("",function(t){
+tape("aa",function(t){
   t.deepEqual(1,1,"any thing");
   t.end();
 });
