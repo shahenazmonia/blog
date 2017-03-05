47afe258bb03bfba2394098f50663ee67b5c8bd5
shahenaz Monia <shahy.m.93@gmail.com>
2017-03-05 23:59:09 +0200

    update test.js

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ const tape = require('tape');
 const shot = require('shot');
 const main = require('../main.js');
 
-tape("",function(t){
+shot("",function(t){
   t.deepEqual(1,1,"any thing");
   t.end();
 });
